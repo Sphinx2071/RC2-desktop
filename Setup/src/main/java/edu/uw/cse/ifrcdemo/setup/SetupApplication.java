569035b0ae946d3baa975749d63a00c5a3237fa3
tvprice <tvprice84@gmail.com>
2024-07-10 10:54:49 -0400

    Removed old setup, added springframework

---
+++ Setup/src/main/java/edu/uw/cse/ifrcdemo/setup/SetupApplication.java
@@ -0,0 +1,42 @@
+package edu.uw.cse.ifrcdemo.setup;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.MethodParameter;
+
+import java.lang.reflect.Field;
+
+@SpringBootApplication
+public class SetupApplication {
+    private static final Logger logger = LogManager.getLogger(SetupApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetupApplication.class, args);
+    }
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    public Logger logger(InjectionPoint injectionPoint) {
+        Class<?> loggingClass = SetupApplication.class;
+
+        MethodParameter methodParameter = injectionPoint.getMethodParameter();
+        if (methodParameter != null) {
+            // constructor injection
+            loggingClass = methodParameter.getContainingClass();
+        } else {
+            Field field = injectionPoint.getField();
+
+            if (field != null) {
+                // field injection
+                loggingClass = field.getDeclaringClass();
+            }
+        }
+        return LogManager.getLogger(loggingClass);
+    }
+}
